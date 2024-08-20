411cca0f9207723e156994e2b92cf1836a4692ac
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-08-20 15:46:53 +0200

    some edits

--- src/components/PopularDestination.jsx
+++ src/components/PopularDestination.jsx
@@ -22,7 +22,7 @@ export const PopularDestination = () => {
                     <div key={element.id}>
                         <img src={element.image} alt=''></img>
                         <div>
-                            <h4>{element.name}</h4>
+                            <h4>{element.name}, {element.country}</h4>
                             <h4>{element.description}</h4>
                         </div> 
 
